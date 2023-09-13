77e6251c2c9a0ef5c1d9a3160e41972e523c568c
Grzegorz1966 <143898309+Grzegorz1966@users.noreply.github.com>
2023-09-14 01:07:46 +0200

    Zadanie z dnia 4

--- PierwszyProjekt/PierwszyProjekt/Program.cs
+++ PierwszyProjekt/PierwszyProjekt/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("To jest zadanie z 4 dnia nauki");
+var imię = "Ewa";
+var płeć = "kobieta";
+var wiek = 33;
+
+if (imię == "Ewa" && wiek == 33) 
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+ 
+{
+ }
+if (płeć == "kobieta" && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+if (płeć != "kobieta" && wiek < 18)
+{
+    Console.WriteLine("Jesteś niepełnoletnim mężczyzną"); 
+ }
